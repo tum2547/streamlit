565dc51ddcfe6d16e5d92c9b50cec9c12deb3c54
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-01-16 15:32:27 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -20,4 +20,8 @@ df2 = df.groupby('species')['body_mass_g'].mean()
 st.write(df2)
 st.bar_chart(df2)
 
-st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('Comedy', 'Drama', 'Documentary'))
+
+st.write(f"You selected {genre}")
