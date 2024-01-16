dfcee11b40b4afc03e741b64b81537693c48988e
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-01-16 14:58:42 +0700

    .

--- Dashboard.py
+++ Dashboard.py
@@ -1,11 +1,11 @@
-import streamlit as streamlit
+import streamlit as st
 import pandas as pd
 
-#สร้างแดชบอร์ด
-st,title("ทดสอบการใช้งาน sstreamlit")
+# สร้างแดชบอร์ด
+st.title("ทดสอบการใช้งาน Streamlit")
 
 st.header("Header")
 st.subheader('Raw data')
 
-#แสดงความ
+# แสดงความ
 st.write("testing")
