1443d24486eefc9b046533c03df8eddf27fad483
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-01-23 15:01:19 +0700

    Update streemlit.py

--- streemlit.py
+++ streemlit.py
@@ -12,4 +12,4 @@ with col2:
 
 with col3:
    st.header("An owl")
-   st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bualabs.com%2Farchives%2F3285%2Fteach-tensorflow-js-build-multi-class-classification-machine-learning-iris-classifier-tabular-data-neural-network-2-dense-layers-tfjs-ep-2%2F&psig=AOvVaw3eqiXsNpf4VUqDywZTjXH5&ust=1706083030574000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNimmN-E84MDFQAAAAAdAAAAABAY")
+   st.image("https://c.pxhere.com/photos/73/a2/potato_flowers_petals_stamens_flower_plant_yellow_rosa_nature-1288559.jpg!d")
